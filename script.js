88d4da8e015fa0a4b6e583c29435186084c5e0b9
Nolekor <pellegrino.nicola90@gmail.com>
2023-01-18 11:48:13 +0100

    add graphic UI

--- script.js
+++ script.js
@@ -1,43 +1,19 @@
-let playerResult = 0;
-let computerResult  = 0;
 const computerSelection = getComputerChoice();
 const playerSelection = getPlayerChoice();
+let playerResult = 0;
+let computerResult  = 0;
 
 function getComputerChoice() {
-    let computerChoice = ["rock", "paper", "scissor"]
+    let computerChoice = ["rock", "paper", "scissor"];
     let randomNumber = Math.floor(Math.random()*computerChoice.length);
-    return computerChoice[randomNumber]
+    return computerChoice[randomNumber];
 }
 
 
-function getPlayerChoice() {
-   let playerChoice = prompt("Pick Rock, Paper or Scissor").toLowerCase()
-   return playerChoice
-}
 
 // basic function to check who win the round, rock paper scissor Game. 
 function resultRound (playerSelection, computerSelection) {
-//     if (playerSelection  == computerSelection) {
-//         return 'Tie'
-//     } else  if (playerSelection == 'rock') {
-//       if (computerSelection == 'paper') {
-//        return "Computer Won"
-//         } else {
-//         return 'Player Won'
-//     }} else if (playerSelection == 'paper') {
-//         if (computerSelection  == 'scissor') {  
-//             return "Computer Won"
-//         } else {
-//         return 'Player Won'
-//     }} else if (playerSelection == 'scissor') {
-//         if (computerSelection == 'rock') {
-//             return "Computer Won"
-//           } else {
-//             return 'Player Won'
-//           }
-//     }
-// }
-    if( playerSelection=='rock' && computerSelection == 'scissor' ||
+     if(playerSelection=='rock' && computerSelection == 'scissor' ||
         playerSelection=='scissor'  && computerSelection=='paper' ||
         playerSelection=='paper' && computerSelection  =='rock') {
             return  'Player Won'
@@ -45,19 +21,3 @@ function resultRound (playerSelection, computerSelection) {
             return 'Tie Game'
         } else {return 'Computer Won'}
     }
-
-function game() {
-    playerResult = 0;
-    computerResult  = 0;
-    roundCount = 0;
-    for (let i = 0; i < 5; i++) {
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-    let gameResult = resultRound(playerChoice,computerChoice);
-    console.log(playerChoice, computerChoice, gameResult)
-        if (gameResult=='Player Won') {playerResult++
-        } else  if (gameResult=='Computer Won') {computerResult++}
-    roundCount++    
-    }
-}
-game()
